07c0bccf37116a867a148a84bbfd7ffb88d0e5c7
Jinho Kim <tiger.ohgirafferes@gmail.com>
2022-10-21 14:49:02 +0900

    #7 hello world 또 추가
    저는 천재 개발자이기 때문에 한 번만 추가한 것이 아니라 당신은 한 번 더 추가하라고 요청할 가능성이 높기 때문에 두 번 출력하는 코드로 추가하였습니다.

--- src/main/java/com/ohgiraffers/workflow/WorkflowApplication.java
+++ src/main/java/com/ohgiraffers/workflow/WorkflowApplication.java
@@ -8,6 +8,8 @@ public class WorkflowApplication {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world2");
+        System.out.println("hello world2");
         SpringApplication.run(WorkflowApplication.class, args);
     }
 
