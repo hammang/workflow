8c1212d3920dbc99d423df80b053bc31e9790053
Jinho Kim <tiger.ohgirafferes@gmail.com>
2022-10-21 14:51:26 +0900

    #7 hello world 또 추가
    다시 수정했습니다

--- src/main/java/com/ohgiraffers/workflow/WorkflowApplication.java
+++ src/main/java/com/ohgiraffers/workflow/WorkflowApplication.java
@@ -9,7 +9,6 @@ public class WorkflowApplication {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world2");
-        System.out.println("hello world2");
         SpringApplication.run(WorkflowApplication.class, args);
     }
 
